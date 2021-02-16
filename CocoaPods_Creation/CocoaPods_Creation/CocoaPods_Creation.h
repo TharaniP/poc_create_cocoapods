03e9f6d228f302c56a5aff503c50a54c6f3b2695
Tharani <tharani.p@dreamorbit.com>
2021-02-16 15:48:53 +0530

    Initial commit

---
+++ CocoaPods_Creation/CocoaPods_Creation/CocoaPods_Creation.h
@@ -0,0 +1,18 @@
+//
+//  CocoaPods_Creation.h
+//  CocoaPods_Creation
+//
+//  Created by Tharani on 2/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaPods_Creation.
+FOUNDATION_EXPORT double CocoaPods_CreationVersionNumber;
+
+//! Project version string for CocoaPods_Creation.
+FOUNDATION_EXPORT const unsigned char CocoaPods_CreationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPods_Creation/PublicHeader.h>
+
+
---
+++ CocoaPods_Creation/CocoaPods_Creation/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  CocoaPods_Creation
+//
+//  Created by Tharani on 2/16/21.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    public static func doSomething() -> String {
+      return "Did some stuff"
+    }
+}
